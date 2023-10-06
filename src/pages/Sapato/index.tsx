5160865fbc1061d9f89b6bc530a9b7ae6095643d
NaraCamargo <naraliggia2@gmail.com>
2023-10-06 11:53:26 -0300

    06/10

--- src/pages/Sapato/index.tsx
+++ src/pages/Sapato/index.tsx
@@ -1,5 +1,4 @@
 import styles from './Sapato.module.scss';
-import Logo from 'assets/logo.png';
 import Buscador from './Buscador';
 import { useState } from 'react';
 import Filtros from './Filtros';
@@ -9,15 +8,11 @@ export default function Sapato(){
     return(
     <main>
         <nav className={styles.modelos}>
-            <img src={Logo} alt="Logo do sapato" className={styles.img}/>
         </nav>
 
-
         <header className={styles.header}>
-            <div className={styles.header__text}>
-             Descontos de 40%
-            </div>
         </header>      
+        
         <section className={styles.sapato}>
             <h3 className={styles.sapato__titulo}>Sapatos na promoção!</h3>
             <Buscador busca={busca} setBusca={setBusca}/> 
